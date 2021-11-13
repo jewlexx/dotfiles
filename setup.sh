d7221e3ebe162ffd2b1a732d5a4c1c0d26240c7b
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-11-13 14:09:12 +1100

    Fixes

---
+++ setup.sh
@@ -0,0 +1,5 @@
+rm $HOME/.zshrc $HOME/.gitconfig $HOME/.p10k.zsh -f
+
+ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/p10k.zsh $HOME/.p10k.zsh
