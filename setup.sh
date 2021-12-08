bc15f8e7d9876b2084f49ab053ff092db9f10076
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-12-08 18:09:39 +1100

    Updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 rm $HOME/.zshrc $HOME/.gitconfig $HOME/.p10k.zsh -f
 
-ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
+export DOTFILES="$HOME/dotfiles"
+
+echo "source $DOTFILES/zshrc" >> $HOME/.zshrc
+echo "source $DOTFILES/p10k.zsh" >> $HOME/.p10k.zsh
+
 ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig
-ln -s $HOME/dotfiles/p10k.zsh $HOME/.p10k.zsh
