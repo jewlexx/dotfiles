b5f6ce0c6cfc7309532c56333382b7ae1a4570cb
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-12-27 16:41:44 +1100

    added ability to clone dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use git2::Repository;
+use home::home_dir;
 use sys_info::linux_os_release;
 
 fn main() {
@@ -8,6 +10,15 @@ fn main() {
     println!("Running on: {}", pretty_name);
 
     if distro != "manjaro" {
-        panic!("Unsupported operating system!");
+        panic!("unsupported operating system!");
     }
+
+    let url = "https://github.com/jamesinaxx/dotfiles.git";
+    let mut clone_dir = home_dir().unwrap();
+    clone_dir.push("dotfiles");
+
+    let repo = match Repository::clone(url, clone_dir) {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to clone: {}", e),
+    };
 }
