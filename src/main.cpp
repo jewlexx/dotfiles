d7fca5903cbfc2fbd2cfae20fbe6e49079faf546
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-12-31 00:16:24 +1100

    YAY

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <git2.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <unistd.h>
+#include <chrono>
+#include <thread>
+
+using std::string;
+
+int clone_dotfiles()
+{
+    passwd *pw = getpwuid(getuid());
+    char *home_dir = pw->pw_dir;
+
+    std::string dotfiles(home_dir);
+    dotfiles.append("/dotfilestemp");
+
+    const char *dotfiles_char = dotfiles.c_str();
+
+    git_repository *repo = NULL;
+
+    std::cout << dotfiles_char << "\n";
+
+    int success = git_clone(&repo, "https://github.com/jamesinaxx/dotfiles.git", dotfiles_char, NULL);
+
+    git_repository_free(repo);
+
+    return success;
+}
+
+int main()
+{
+    git_libgit2_init();
+
+    int success = clone_dotfiles();
+
+    if (success != 0)
+    {
+        std::cout << "Clone failed. Double check if the directory already exists?\n";
+        for (int i = 5; i > 0; i--)
+        {
+            std::cout << "\rContinuing in: " << i << std::flush;
+            sleep(1);
+        }
+        std::cout << "\n";
+    }
+}
