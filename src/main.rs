7c1676ace9ee95cd9b861296c0af2b2bbfa9f378
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2021-12-31 11:50:45 +1100

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use git2::Repository;
 use home::home_dir;
+use reqwest::get;
 use rpassword::read_password_from_tty;
 use spinners::{Spinner, Spinners};
 use std::{
@@ -10,18 +11,26 @@ use std::{
     time::Duration,
 };
 
-fn run_cmd(cmd: &str, err: &str) -> Output {
+async fn get_install_cmd(url: &str) -> String {
+    let out = get(url).await.unwrap().text().await.unwrap();
+
+    println!("{:?}", out);
+
+    format!("\"{}\"", out)
+}
+
+async fn run_cmd(cmd: &str, err: &str) -> Output {
     Command::new("sh").arg("-c").arg(cmd).output().expect(err)
 }
 
-fn get_pacman() -> Result<String, String> {
-    let mut out = run_cmd("command -v pacman", "Something went wrong here");
+async fn get_pacman() -> Result<String, String> {
+    let mut out = run_cmd("command -v pacman", "Something went wrong here").await;
     let mut pacman = "unknown";
 
     if out.stdout.len() != 0 {
         pacman = "pacman";
     } else {
-        out = run_cmd("command -v apt", "Something went wrong here");
+        out = run_cmd("command -v apt", "Something went wrong here").await;
         if out.stdout.len() != 0 {
             pacman = "apt";
         }
@@ -30,8 +39,9 @@ fn get_pacman() -> Result<String, String> {
     Ok(pacman.to_string())
 }
 
-fn main() {
-    let pacman = get_pacman().unwrap();
+#[tokio::main]
+async fn main() {
+    let pacman = get_pacman().await.unwrap();
 
     if pacman == "unknown" {
         panic!("unsupported operating system!");
@@ -102,59 +112,59 @@ fn main() {
         let install_yay_cmd = format!("echo {} | sudo --stdin {} {}", passwd, yay_cmd, program);
         let error_msg = format!("failed to install {}", program);
 
-        run_cmd(&install_yay_cmd, &error_msg);
+        run_cmd(&install_yay_cmd, &error_msg).await;
         sp.stop();
     }
 
-    run_cmd(&installcmd, "failed to upgrade system packages");
+    run_cmd(&installcmd, "failed to upgrade system packages").await;
 
-    run_cmd(
-        "$(curl -fsSL https://get.sdkman.io)",
-        "failed to install sdkman",
-    );
+    let mut cmd = get_install_cmd("https://get.sdkman.io").await;
 
-    run_cmd("sdk install java 17.0.1-open", "failed to install java");
+    run_cmd(&cmd, "failed to install sdkman").await;
 
-    run_cmd("sdk install gradle", "failed to install gradle");
+    run_cmd("sdk install java 17.0.1-open", "failed to install java").await;
+
+    run_cmd("sdk install gradle", "failed to install gradle").await;
 
     sp = Spinner::new(&Spinners::Dots, "Installing oh-my-zsh and friends".into());
 
-    run_cmd(
-        "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)",
-        "failed to install oh-my-zsh",
-    );
+    cmd = get_install_cmd(
+        "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh",
+    )
+    .await;
 
-    Repository::clone(
+    run_cmd(&cmd, "failed to install oh-my-zsh").await;
+
+    let mut _err = Repository::clone(
         "https://github.com/romkatv/powerlevel10k.git",
         "~/.oh-my-zsh/custom/themes/powerlevel10k",
     )
-    .expect("failed to install p10k");
+    .err();
 
-    Repository::clone(
+    _err = Repository::clone(
         "https://github.com/zsh-users/zsh-autosuggestions.git",
         "~/.oh-my-zsh/custom/plugins/zsh-autosuggestions",
     )
-    .expect("failed to install suggestions");
+    .err();
 
-    Repository::clone(
+    _err = Repository::clone(
         "https://github.com/zsh-users/zsh-syntax-highlighting.git",
         "~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting",
     )
-    .expect("failed to install syntax highlighting");
+    .err();
 
     sp.stop();
 
     sp = Spinner::new(&Spinners::Dots, "Installing NodeJS".into());
 
-    run_cmd(
-        "$(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh)",
-        "failed to install nvm",
-    );
+    cmd = get_install_cmd("https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh").await;
 
-    run_cmd("nvm install --lts", "failed to install lts NodeJS");
-    run_cmd("nvm install node", "failed to install latest NodeJS");
-    run_cmd("nvm use --lts", "failed to set to use lts NodeJS");
-    run_cmd("npm i -g yarn", "failed to install yarn globally");
+    run_cmd(&cmd, "failed to install nvm").await;
+
+    run_cmd("nvm install --lts", "failed to install lts NodeJS").await;
+    run_cmd("nvm install node", "failed to install latest NodeJS").await;
+    run_cmd("nvm use --lts", "failed to set to use lts NodeJS").await;
+    run_cmd("npm i -g yarn", "failed to install yarn globally").await;
 
     sp.stop();
 
@@ -163,7 +173,7 @@ fn main() {
         passwd
     );
 
-    run_cmd(&ln_cmd, "failed to symlink for classic snaps");
+    run_cmd(&ln_cmd, "failed to symlink for classic snaps").await;
 
     let mut dir = home_dir().unwrap();
 
@@ -178,16 +188,17 @@ fn main() {
 
     sp = Spinner::new(&Spinners::Dots, "Installing VSCode".into());
 
-    run_cmd("snap install code --classic", "failed to install vscode");
+    run_cmd("snap install code --classic", "failed to install vscode").await;
 
     sp.stop();
 
     run_cmd(
         "rm $HOME/.zshrc $HOME/.gitconfig $HOME/.p10k.zsh -f",
         "failed to remove old config files",
-    );
+    )
+    .await;
 
-    run_cmd("ln -s $HOME/dotfiles/zshrc $HOME/.zshrc && ln -s $HOME/dotfiles/p10k.zsh $HOME/.p10k.zsh && ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig", "failed to symlink config files");
+    run_cmd("ln -s $HOME/dotfiles/zshrc $HOME/.zshrc && ln -s $HOME/dotfiles/p10k.zsh $HOME/.p10k.zsh && ln -s $HOME/dotfiles/gitconfig $HOME/.gitconfig", "failed to symlink config files").await;
 
     println!("\nFinished! Go have some fun :)");
 }
