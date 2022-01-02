2eafa7264c5b4b2006f66bf2febae2740b4ecd12
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2022-01-02 16:20:47 +1100

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,29 @@
 #include <sys/utsname.h>
 #include <string>
 #include <vector>
+#include <termios.h>
+#include <unistd.h>
 
 using std::cout;
 using std::endl;
+using std::getline;
 using std::string;
 using std::vector;
 
-void get_os(vector<string> *details)
+// Copied from http://stackoverflow.com/
+static void SetStdinEcho(bool enable = true)
+{
+    struct termios tty;
+    tcgetattr(STDIN_FILENO, &tty);
+    if (!enable)
+        tty.c_lflag &= ~ECHO;
+    else
+        tty.c_lflag |= ECHO;
+
+    (void)tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+}
+
+static void get_os(vector<string> *details)
 {
     utsname name;
     uname(&name);
@@ -27,4 +43,11 @@ int main()
     const string os = details[1];
 
     cout << "Running on " << os << " v" << version << endl;
+
+    string password;
+    cout << "Please enter the root password: ";
+    SetStdinEcho(false);
+    getline(std::cin, password);
+    SetStdinEcho();
+    cout << "\rThank you :)" << endl;
 }
