83962b3ad17e6d51d8c0f9f6b4835c4da3fe9d24
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2022-01-11 13:22:41 +1100

    fix

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ fn cmd_exists(cmd: &str) -> bool {
     Command::new(cmd).output().is_ok()
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     if !cmd_exists("apt") {
         panic!("apt is not available")
     }
