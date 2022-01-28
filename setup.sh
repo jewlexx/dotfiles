5bf5a98df5c2c584a405315afe7f9f78dd3d9735
jamesinaxx <61040004+jamesinaxx@users.noreply.github.com>
2022-01-28 13:44:10 +1100

    fix

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 sudo pacman-mirrors --fasttrack && sudo pacman -Syyu
 sudo pacman -S code base-devel nvidia p7zip git
 
+code --install-extension Shan.code-settings-sync
+
 g++ `pwd`/src/main.cpp
 ./a.out
 
 sh -c `curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh`
 sh -c `curl -fsSL https://get.sdkman.io`
+sh -c `curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh`
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm install --lts
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
