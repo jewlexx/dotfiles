9ae49b4cf9a66e687d23589ff0d908861bb0802a
Juliet Cordor <grassojames5@gmail.com>
2022-02-27 10:52:44 +1100

    removed rust program

--- setup/src/utils/consts.rs
+++
@@ -1,9 +0,0 @@
-pub const URL: &str = "https://github.com/jewlexx/dotfiles.git";
-
-#[cfg(target_os = "windows")]
-pub const CMD: &str = "cmd.exe";
-
-#[cfg(not(target_os = "windows"))]
-pub const CMD: &str = "sh";
-
-pub const DOTFILES: [&str; 3] = ["zshrc", "gitconfig", "gitignore"];
--- setup/src/utils/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod consts;
-pub mod repo;
-pub mod system;
