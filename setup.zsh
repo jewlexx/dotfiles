287a31a50fd1a70380804fdfc1160b2e1a9c362d
Juliet Cordor <grassojames5@gmail.com>
2022-02-27 11:11:52 +1100

    fix

--- setup.zsh
+++ setup.zsh
@@ -8,10 +8,6 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 nvm install node
 nvm alias default node
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 cd ~
 
 rm ~/.zshrc -f
