769746f3e4fcbe72532b0c9f346e876387cfb86e
Juliet Cordor <grassojames5@gmail.com>
2022-02-28 21:07:15 +1100

    fx

--- configs/rc.zsh
+++ configs/rc.zsh
@@ -10,7 +10,7 @@ export TOOLS="$HOME/Tools"
 export ZSH="$HOME/.oh-my-zsh"
 export DENO_INSTALL="$HOME/.deno"
 export NVM_DIR="$HOME/.nvm"
-export PATH="$PATH:$DENO_INSTALL/bin:$HOME/bin:$HOME/spicetify-cli:$TOOLS/bin:/usr/local/go/bin:$HOME/.pub-cache/bin:$HOME/.local/bin"
+export PATH="$PATH:$DENO_INSTALL/bin:$HOME/bin:$HOME/spicetify-cli:$TOOLS/bin:/usr/local/go/bin:$HOME/.pub-cache/bin:$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.dotnet"
 export SDKMAN_DIR="$HOME/.sdkman"
 export CHROME_EXECUTABLE="google-chrome-stable"
 
