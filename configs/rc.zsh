32d346986b811ebb214ee9e0fdfc260d2bb5ef37
Juliette Cordor <grassojames5@gmail.com>
2022-03-08 20:03:18 +1100

    moved vars to seperate file

--- configs/rc.zsh
+++ configs/rc.zsh
@@ -1,18 +1,3 @@
-# Init variables
-## Forces terminal to be English
-export LC_ALL=C
-export GPG_TTY=$(tty)
-export SHELL="/bin/zsh"
-export DOTFILES="$HOME/dotfiles"
-export TOOLS="$HOME/Tools"
-export ZSH="$HOME/.oh-my-zsh"
-export DENO_INSTALL="$HOME/.deno"
-export NVM_DIR="$HOME/.nvm"
-export PATH="$PATH:$DENO_INSTALL/bin:$HOME/bin:$HOME/spicetify-cli:$TOOLS/bin:/usr/local/go/bin:$HOME/.pub-cache/bin:$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.dotnet"
-export SDKMAN_DIR="$HOME/.sdkman"
-export CHROME_EXECUTABLE="google-chrome-stable"
-export STARSHIP_CONFIG="$HOME/dotfiles/configs/starship.toml"
-
 plugins=(
   zsh-syntax-highlighting
   zsh-autosuggestions
@@ -21,6 +6,7 @@ plugins=(
   git
 )
 
+source $HOME/dotfiles/utils/vars.zsh
 source $DOTFILES/utils/commands.zsh
 source $ZSH/oh-my-zsh.sh
 
---
+++ utils/vars.zsh
@@ -0,0 +1,14 @@
+# Init variables
+## Forces terminal to be English
+export LC_ALL=C
+export GPG_TTY=$(tty)
+export SHELL="/bin/zsh"
+export DOTFILES="$HOME/dotfiles"
+export TOOLS="$HOME/Tools"
+export ZSH="$HOME/.oh-my-zsh"
+export DENO_INSTALL="$HOME/.deno"
+export NVM_DIR="$HOME/.nvm"
+export PATH="$PATH:$DENO_INSTALL/bin:$HOME/bin:$HOME/spicetify-cli:$TOOLS/bin:/usr/local/go/bin:$HOME/.pub-cache/bin:$HOME/.cargo/bin:$HOME/.local/bin:$HOME/.dotnet"
+export SDKMAN_DIR="$HOME/.sdkman"
+export CHROME_EXECUTABLE="google-chrome-stable"
+export STARSHIP_CONFIG="$HOME/dotfiles/configs/starship.toml"
