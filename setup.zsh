78b431de51674148fff289bf3c58acb662d9bb40
Juliette Cordor <grassojames5@gmail.com>
2022-03-11 11:24:24 +1100

    added ohmyzsh

--- setup.zsh
+++ setup.zsh
@@ -2,18 +2,13 @@ source $HOME/.dotfiles/utils/vars.zsh
 
 git submodule init && git submodule update
 
-sh -C `curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh`
-sh -C `curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh`
-sh -C `curl -fsSL https://get.sdkman.io`
-
-curl -fsSL https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.sh | sh
-curl -fsSL https://raw.githubusercontent.com/CharlieS1103/spicetify-marketplace/main/install.sh | sh
+sh -c `curl -fsSL https://get.sdkman.io`
 
 export NVM_DIR="`[ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm"`"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-nvm install node
-nvm alias default node
+nvm install --lts
+nvm alias default --lts
 npm i -g yarn typescript ts-node npkill
 
 cd ~
