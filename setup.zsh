490dd189b3a0841e06c86e95f086fe9c9623a899
Juliette Cordor <grassojames5@gmail.com>
2022-03-12 20:30:21 +1100

    updates

--- setup.zsh
+++ setup.zsh
@@ -1,25 +1,19 @@
+#!/usr/bin/zsh
+
 source $HOME/.dotfiles/utils/vars.zsh
 
-git submodule init && git submodule update
+curl -fsSL "https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh" | sh
+curl -fsSL "https://get.sdkman.io" | sh
+curl -fsSL "https://sh.rustup.rs" | sh
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.9.0
-sh -c `curl -fsSL https://get.sdkman.io`
-sh -c `curl -fsSL https://sh.rustup.rs`
 
+source $HOME/.cargo/env
 source $HOME/.asdf/asdf.sh
 
 rustup install stable
 rustup install nightly
-
-# Install nodejs
-asdf plugin add nodejs
-asdf install nodejs lts
-asdf global nodejs lts
-
-# Other plugins
-asdf plugin add ruby
-asdf plugin add deno
 
 cd $HOME
 
@@ -29,7 +23,7 @@ rm $HOME/.zshrc -f
 rm $HOME/.p10k.zsh -f
 rm $HOME/.gitconfig -f
 rm $HOME/.config/alacritty/alacritty.yml -f
-rm $HOME/.default-npm-packages
+rm $HOME/.default-npm-packages -f
 
 ln -s $DOTFILES/configs/rc.zsh $HOME/.zshrc
 ln -s $DOTFILES/configs/p10k.zsh $HOME/.p10k.zsh
@@ -38,3 +32,12 @@ ln -s $DOTFILES/configs/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 ln -s $DOTFILES/configs/default-npm $HOME/.default-npm-packages
 
 sudo cp $DOTFILES/fonts/*/*.ttf $HOME/.local/share/fonts
+
+# Install nodejs
+asdf plugin add nodejs
+asdf install nodejs lts
+asdf global nodejs lts
+
+# Other plugins
+asdf plugin add ruby
+asdf plugin add deno
