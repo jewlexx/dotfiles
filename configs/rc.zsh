de08eff537a75c9e6186dd526bc5c365a907ebd7
Juliette Cordor <grassojames5@gmail.com>
2022-03-26 18:24:44 +1100

    fixes

--- configs/rc.zsh
+++ configs/rc.zsh
@@ -28,3 +28,6 @@ autoload -Uz compinit && compinit
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# Setup Java home variable
+. ~/.asdf/plugins/java/set-java-home.zsh
