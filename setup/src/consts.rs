e375011be386f6c8187cefb2cf7dc65da552798e
Juliette Cordor <grassojames5@gmail.com>
2022-04-02 14:02:04 +1100

    fixes

--- setup/src/consts.rs
+++ setup/src/consts.rs
@@ -3,6 +3,7 @@ pub const URL: &str = "https://github.com/jewlexx/dotfiles.git";
 pub const fn is_win() -> bool {
     cfg!(target_os = "windows")
 }
+
 pub const fn is_dev() -> bool {
     cfg!(debug_assertions)
 }
