a04d7fdafce8104f4993622bcb48256627eeaf65
Juliette Cordor <grassojames5@gmail.com>
2022-04-05 16:18:57 +1000

    fixes

--- configs/rc.zsh
+++ configs/rc.zsh
@@ -31,3 +31,7 @@ autoload -Uz compinit && compinit
 
 # Setup Java home variable
 . ~/.asdf/plugins/java/set-java-home.zsh
+
+# Wasmer
+export WASMER_DIR="$HOME/.wasmer"
+[ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
