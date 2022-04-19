664581a24a2e8efc336ba6d21dd62594d267ecc5
Juliette Cordor <grassojames5@gmail.com>
2022-04-19 13:55:19 +1000

    fixes

--- setup.sh
+++ setup.sh
@@ -65,8 +65,7 @@ asdf install deno latest
 asdf global deno latest
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # Install NodeJS
 nvm install --lts
