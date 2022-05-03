b52ae1d83157897d42c43239ab1a65b8069ff2df
Juliette Cordor <grassojames5@gmail.com>
2022-05-04 09:14:56 +1000

    fixes

--- setup/src/main.rs
+++ setup/src/main.rs
@@ -1,11 +1,19 @@
 use crate::utils::admin::IS_ELEVATED;
+use anyhow::{anyhow, Result};
 
 mod utils;
 
 const GIT_URL: &str = "https://github.com/jewlexx/dotfiles.git";
 
-fn main() {
-    let c = *IS_ELEVATED;
+fn main() -> Result<()> {
+    #[cfg(target_os = "windows")]
+    {
+        if !*IS_ELEVATED {
+            Err(anyhow!("Please run this program as administrator."))
+        } else {
+            Ok(())
+        }
+    }?;
 
-    println!("Elevated? {}", c);
+    Ok(()c)
 }
