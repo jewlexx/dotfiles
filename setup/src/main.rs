350d204262c11a91299c632ca30e1433436d49b6
Juliette Cordor <grassojames5@gmail.com>
2022-05-04 09:15:33 +1000

    fixes

--- setup/src/main.rs
+++ setup/src/main.rs
@@ -9,7 +9,9 @@ fn main() -> Result<()> {
     #[cfg(target_os = "windows")]
     {
         if !*IS_ELEVATED {
-            Err(anyhow!("Please run this program as administrator."))
+            Err(anyhow!(
+                "Because you are on Windows, you must run this as admin. Sorry ik it sucks :("
+            ))
         } else {
             Ok(())
         }
