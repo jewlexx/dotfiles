4e78eec0681e28a791545c7d5416af0e9eed44a6
Juliette Cordor <grassojames5@gmail.com>
2022-05-04 11:19:46 +1000

    disabled async

--- setup/src/utils/git.rs
+++ setup/src/utils/git.rs
@@ -2,6 +2,6 @@ use git2::Repository;
 
 use crate::{utils::fs::DOTFILES, GIT_URL};
 
-pub async fn clone_repo() -> smol::Task<Result<Repository, git2::Error>> {
+pub fn clone_repo() -> smol::Task<Result<Repository, git2::Error>> {
     smol::spawn(async { Repository::clone(GIT_URL, DOTFILES.clone()) })
 }
