f6156624ca00cb43d414c8f6b48a9eb7aca5a2f9
Juliette Cordor <grassojames5@gmail.com>
2022-05-04 19:29:31 +1000

    fixes

--- setup/src/main.rs
+++ setup/src/main.rs
@@ -1,7 +1,4 @@
-use crate::utils::{admin::IS_ELEVATED, git::clone_repo};
-
-#[macro_use]
-extern crate anyhow;
+use crate::utils::git::clone_repo;
 
 #[macro_use]
 extern crate lazy_static;
@@ -10,17 +7,6 @@ mod sys;
 mod utils;
 
 fn main() -> anyhow::Result<()> {
-    // #[cfg(target_os = "windows")]
-    // {
-    //     if !*IS_ELEVATED {
-    //         Err(anyhow!(
-    //             "Because you are on Windows, you must run this as admin. Sorry ik it sucks :("
-    //         ))
-    //     } else {
-    //         Ok(())
-    //     }
-    // }?;
-
     println!("Started cloning repository...");
     let repo_task = clone_repo();
 
