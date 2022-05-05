360c30dcd45a403bfdb16dc5cb79afa677f0b11e
Juliette Cordor <grassojames5@gmail.com>
2022-05-05 11:46:54 +1000

    fixes

--- setup/src/utils/git.rs
+++ setup/src/utils/git.rs
@@ -5,9 +5,9 @@ use crate::utils::fs::DOTFILES;
 pub const GIT_URL: &str = "https://github.com/jewlexx/dotfiles.git";
 
 pub fn clone_repo() -> smol::Task<Result<Repository, git2::Error>> {
-    let r = smol::spawn(async { Repository::clone(GIT_URL, DOTFILES.clone()) });
-
-    println!("Finished cloning repository!");
-
-    r
+    smol::spawn(async {
+        let r = Repository::clone(GIT_URL, DOTFILES.clone());
+        println!("Finished cloning repository!");
+        r
+    })
 }
