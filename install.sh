20f1819cd71c435a9447ebc72bce98efca199274
Juliette Cordor <grassojames5@gmail.com>
2022-05-23 11:13:48 +1000

    updates

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
 if command -v apt &>/dev/null; then
+    if ! command -v code &>/dev/null; then
+        snap install code --classic
+    fi
+
+    sudo apt-get update
+    sudo apt-get upgrade -y
+
+    sudo apt-get install build-essential -y
 fi
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+rustup install nightly
+rustup default stable
+
+cargo install cargo-edit
+cargo install ignoreit
