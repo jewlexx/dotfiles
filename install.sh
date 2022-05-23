4e06f9af719d34c0e3c9509a7f7142494f47a334
Juliette Cordor <grassojames5@gmail.com>
2022-05-23 11:23:30 +1000

    fixes

--- install.sh
+++ install.sh
@@ -16,6 +16,8 @@ curl -fsSL "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh" | s
 curl -fsSL "https://sh.rustup.rs" | sh -s -- -y
 curl -fsSL "https://rustwasm.github.io/wasm-pack/installer/init.sh" | sudo sh
 
+source $HOME/.cargo/env
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
