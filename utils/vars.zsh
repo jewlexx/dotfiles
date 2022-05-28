f7d6a82abee413d1bfd79da31b21221c61a87f0a
Juliette Cordor <grassojames5@gmail.com>
2022-05-28 14:12:54 +1000

    fixes

--- utils/vars.zsh
+++ utils/vars.zsh
@@ -2,14 +2,15 @@
 ## Forces terminal to be English
 export LC_ALL=C
 
-# Paths
-export PATH="$PATH:$HOME/bin:$HOME/spicetify-cli:$HOME/.tools/bin:$HOME/.cargo/bin"
-
 # Simple variables
 export DOTFILES="$HOME/.dotfiles"
 export SHELL="/bin/zsh"
 export ZSH="$HOME/.oh-my-zsh"
 export CHROME_EXECUTABLE="google-chrome-stable"
+export DENO_INSTALL="/home/juliette/.deno"
+
+# Paths
+export PATH="$HOME/bin:$HOME/spicetify-cli:$HOME/.tools/bin:$HOME/.cargo/bin:$DENO_INSTALL/bin:$PATH"
 
 # Ensures that gpg uses my tty for the password
 export GPG_TTY=$TTY
