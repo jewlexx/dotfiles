d04c81d5c670946892f78c1c337f9cee3a3d0202
Juliette Cordor <grassojames5@gmail.com>
2022-10-13 13:02:22 +1100

    updated pwsh script

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,9 @@
+function createSymbolic([string]$source, [string]$target) {
+    if (Test-Path $target) {
+        Remove-Item $target
+    }
+
+    New-Item -Type symboliclink -Target $source -Path $target
+}
+
 Remove-Item "$HOME\.gitconfig" -Force
-New-Item -Type symboliclink -Target "$HOME\.dotfiles\configs\git.win.properties" -Path "$HOME\.gitconfig"
