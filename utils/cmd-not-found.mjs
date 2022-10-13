d7bac4f245e7ad6bdbc144f96d4ac9620f6471e6
Juliette Cordor <grassojames5@gmail.com>
2022-10-13 12:47:50 +1100

    fix

--- utils/cmd-not-found.mjs
+++ utils/cmd-not-found.mjs
@@ -9,3 +9,7 @@ const response = await $`pkgfile -b -v -- "${cmd}"`;
  * @type {string}
  */
 const packages = response.stdout;
+
+console.log(
+  `The application ${cmd} is not installed. It may be found in the following packages:\n`,
+);
