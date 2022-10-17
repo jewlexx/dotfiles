321c664f61b7fde73159219109a99337e9b7349b
Juliette Cordor <grassojames5@gmail.com>
2022-10-17 21:20:55 +1100

    fixes

--- setup.ps1
+++ setup.ps1
@@ -1,18 +1,28 @@
-function local:createSymbolic([string]$source, [string]$target) {
-    if (Test-Path $target) {
-        Remove-Item $target
-    }
+# function local:createSymbolic([string]$source, [string]$target) {
+#     if (Test-Path $target) {
+#         Remove-Item $target
+#     }
 
-    New-Item -Type symboliclink -Target $source -Path $target
-}
+#     New-Item -Type symboliclink -Target $source -Path $target
+# }
 
-createSymbolic "$DOTFILES/zshrc.sh" "$HOME/.zshrc"
-createSymbolic "$DOTFILES/configs/p10k.sh" "$HOME/.p10k.zsh"
-createSymbolic "$DOTFILES/configs/git.nix.properties" "$HOME/.gitconfig"
-createSymbolic "$DOTFILES/configs/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
-createSymbolic "$DOTFILES/configs/default-npm" "$HOME/.default-npm-packages"
-createSymbolic "$DOTFILES/configs/vimrc.vim" "$HOME/.vimrc"
-createSymbolic "$DOTFILES/configs/init.vim" "$HOME/.config/nvim/init.vim"
+# createSymbolic "$DOTFILES/zshrc.sh" "$HOME/.zshrc"
+# createSymbolic "$DOTFILES/configs/p10k.sh" "$HOME/.p10k.zsh"
+# createSymbolic "$DOTFILES/configs/git.nix.properties" "$HOME/.gitconfig"
+# createSymbolic "$DOTFILES/configs/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
+# createSymbolic "$DOTFILES/configs/default-npm" "$HOME/.default-npm-packages"
+# createSymbolic "$DOTFILES/configs/vimrc.vim" "$HOME/.vimrc"
+# createSymbolic "$DOTFILES/configs/init.vim" "$HOME/.config/nvim/init.vim"
 
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
-Invoke-RestMethod get.scoop.sh | Invoke-Expression
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
+# Invoke-RestMethod get.scoop.sh | Invoke-Expression
+
+# scoop bucket add java
+# scoop bucket add extras
+
+scoop install `
+    android-studio `
+    android-sdk `
+    delta `
+    ungoogled-chromium `
+    which
