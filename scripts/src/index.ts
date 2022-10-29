b7e0c5b583890e5b5270d7bf6642f95673e450ab
Juliette Cordor <grassojames5@gmail.com>
2022-10-29 15:31:30 +1100

    fixes

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -2,9 +2,8 @@ import backup from './scoop/backup';
 
 const args = process.argv.slice(2);
 
-if (args[0] == 'backup') {
-  await backup();
-}
-
-// Ensure this file is considered a module
-export {};
+(async () => {
+  if (args[0] == 'backup') {
+    await backup();
+  }
+})();
