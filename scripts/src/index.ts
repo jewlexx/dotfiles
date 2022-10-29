d6bcb331f6f3e16554288e08155e21b5a016bbfe
Juliette Cordor <grassojames5@gmail.com>
2022-10-29 15:27:13 +1100

    updated backup process

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,7 +1,9 @@
+import backup from './scoop/backup';
+
 const args = process.argv.slice(2);
 
 if (args[0] == 'backup') {
-  await import('./scoop/backup');
+  await backup();
 }
 
 // Ensure this file is considered a module
--- scripts/src/scoop/backup.ts
+++ scripts/src/scoop/backup.ts
@@ -1,25 +1,25 @@
 import { $ } from 'zx';
-import fs from 'fs';
+import fs from 'fs/promises';
 
 import { Convert as PackageConvert } from './packages';
 import { Convert as BucketConvert } from './buckets';
 
-const pkgsJsonOutput = await $`scoop list | ConvertTo-Json`;
+export default async function backup() {
+  const pkgsJsonOutput = await $`scoop list | ConvertTo-Json`;
 
-const packages = PackageConvert.toPackages(pkgsJsonOutput.stdout);
+  const packages = PackageConvert.toPackages(pkgsJsonOutput.stdout);
 
-const packageBackup = packages.map((pkg) => pkg.Name).join('\n');
+  const packageBackup = packages.map((pkg) => pkg.Name).join('\n');
 
-fs.writeFileSync('../scoop-packages.txt', packageBackup);
+  const bucketsJsonOutput = await $`scoop bucket list | ConvertTo-Json`;
 
-const bucketsJsonOutput = await $`scoop bucket list | ConvertTo-Json`;
+  const buckets = BucketConvert.toBuckets(bucketsJsonOutput.stdout);
 
-const buckets = BucketConvert.toBuckets(pkgsJsonOutput.stdout);
+  const bucketsBackup = buckets
+    .map((bucket) => `${bucket.Name} ${bucket.Source}`)
+    .join('\n');
 
-const bucketsBackup = packages
-  .map((bucket) => `${bucket.Name} ${bucket.Source}`)
-  .join('\n');
+  await fs.writeFile('../scoop-packages.txt', packageBackup);
 
-fs.writeFileSync('../scoop-packages.txt', packageBackup);
-
-fs.writeFileSync('../scoop-buckets.txt', bucketsBackup);
+  await fs.writeFile('../scoop-buckets.txt', bucketsBackup);
+}
