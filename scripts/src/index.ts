7e346fad6db82161f1d1222f96552c44ab43a380
Juliette Cordor <grassojames5@gmail.com>
2022-10-29 15:15:12 +1100

    added backup scoop script

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -1,3 +1,29 @@
 import { $ } from 'zx';
+import fs from 'fs';
+
+export interface Packages {
+  Version: string;
+  Source: string;
+  Info: string;
+  Name: string;
+  Updated: Date;
+}
+
+// Converts JSON strings to/from your types
+export class Convert {
+  public static toPackages(json: string): Packages[] {
+    return JSON.parse(json);
+  }
+
+  public static packagesToJson(value: Packages[]): string {
+    return JSON.stringify(value);
+  }
+}
 
 const jsonOutput = await $`scoop list | ConvertTo-Json`;
+
+const packages = Convert.toPackages(jsonOutput.stdout);
+
+const packageBackup = packages.map((pkg) => pkg.Name).join('\n');
+
+fs.writeFileSync('../scoop-packages.txt', packageBackup);
