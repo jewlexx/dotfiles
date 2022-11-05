4816025926be5df964f808130c00dd4c960e0abb
Juliette Cordor <grassojames5@gmail.com>
2022-11-05 18:16:53 +1100

    update setup

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,14 @@
-function local:createSymbolic([string]$source, [string]$target) {
+function local:Test-Version {
+    $Major = $Host.Version.Major
+
+    if ($Major -lt 7) {
+        throw "PowerShell 7.0 or higher is required"
+    }
+}
+
+Test-Version
+
+function local:New-Symbolic([string]$source, [string]$target) {
     if (Test-Path $target) {
         Remove-Item $target
     }
@@ -8,9 +18,9 @@ function local:createSymbolic([string]$source, [string]$target) {
 
 $DOTFILES = "$HOME/.dotfiles"
 
-createSymbolic "$DOTFILES/configs/git.win.properties" "$HOME/.gitconfig"
-createSymbolic "$DOTFILES/configs/vimrc.vim" "$HOME/.vimrc"
-createSymbolic "$DOTFILES/configs/init.vim" "$HOME/.config/nvim/init.vim"
+New-Symbolic "$DOTFILES/configs/git.win.properties" "$HOME/.gitconfig"
+New-Symbolic "$DOTFILES/configs/vimrc.vim" "$HOME/.vimrc"
+New-Symbolic "$DOTFILES/configs/init.vim" "$HOME/.config/nvim/init.vim"
 
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
 Invoke-RestMethod get.scoop.sh | Invoke-Expression
