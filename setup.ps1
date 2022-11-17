ecf87cd1a2d1b892a39d1723db12ea8bdf76a6f9
Juliette Cordor <grassojames5@gmail.com>
2022-11-17 18:21:49 +1100

    fixes

--- setup.ps1
+++ setup.ps1
@@ -1,26 +1,36 @@
-function local:Test-Version {
+function local:Test-Version
+{
     $Major = $Host.Version.Major
 
-    if ($Major -lt 7) {
-        Write-Output "Installing Powershell 7"
+    if ($Major -lt 7)
+    {
+        Write-Output 'Installing Powershell 7'
 
         scoop install powershell
     }
 }
 
-function local:New-Symbolic([string]$source, [string]$target) {
-    if (Test-Path $target) {
+function local:New-Symbolic([string]$source, [string]$target)
+{
+    if (Test-Path $target)
+    {
         Remove-Item $target
     }
 
     New-Item -Type symboliclink -Target $source -Path $target
 }
 
+Invoke-WebRequest -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    New-Item $HOME/vimfiles/autoload/plug.vim -Force
+
 $DOTFILES = "$HOME/.dotfiles"
 
 New-Symbolic "$DOTFILES/configs/git.win.properties" "$HOME/.gitconfig"
 New-Symbolic "$DOTFILES/configs/vimrc.vim" "$HOME/.vimrc"
-New-Symbolic "$DOTFILES/configs/init.vim" "$HOME/.config/nvim/init.vim"
+
+# Ensure nvim location exists
+New-Item "$HOME/AppData/Local/nvim" -Type Directory
+New-Symbolic "$DOTFILES/configs/init.vim" "$HOME/AppData/Local/nvim/init.vim"
 
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser # Optional: Needed to run a remote script the first time
 Invoke-RestMethod get.scoop.sh | Invoke-Expression
@@ -30,12 +40,14 @@ Test-Version
 $Buckets = Get-Content .\scoop-buckets.json | ConvertFrom-Json
 $Packages = Get-Content .\scoop-packages.json | ConvertFrom-Json
 
-ForEach ($Bucket in $Buckets) {
+ForEach ($Bucket in $Buckets)
+{
     scoop bucket add $Bucket.Name $Bucket.Url
 }
 
 
-ForEach ($Package in $Packages) {
+ForEach ($Package in $Packages)
+{
     scoop install $Package
 }
 
