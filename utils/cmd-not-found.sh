6a098e2c87dae90fc89e37003c1e0e6525888e1e
Juliette Cordor <grassojames5@gmail.com>
2023-02-28 18:56:11 +1100

    fix

--- utils/cmd-not-found.sh
+++ utils/cmd-not-found.sh
@@ -3,7 +3,5 @@
 export PKGFILE_PROMPT_INSTALL_MISSING=1
 
 command_not_found_handler() {
-  local cmd="$1"
-
-  zx "$DOTFILES/utils/cmd-not-found.mjs" "$cmd"
+  zx "$DOTFILES/utils/cmd-not-found.mjs" "$1"
 }
