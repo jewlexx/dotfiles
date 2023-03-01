b43c8d60221fc21fd1af5674a555ecd1e4d5a7a8
Juliette Cordor <juliette@jewelexx.com>
2023-03-01 15:00:15 +1100

    use scoop export rather than seperate files

---
+++ scripts/restore.ps1
@@ -0,0 +1,18 @@
+# Restores the Scoop export from the above directory
+
+$ExportPath = "$PSScriptRoot../scoop-export.json"
+
+$Export = Get-Content $ExportPath | ConvertFrom-Json
+
+ForEach ($Bucket in $Export.buckets)
+{
+    scoop bucket add $Bucket.Name $Bucket.Source
+}
+
+ForEach ($App in $Export.apps)
+{
+    $Source = $App.Source
+    $Name = $App.Name
+
+    scoop install "$Source/$Name"
+}
