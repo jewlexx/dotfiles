38ff309d3c12b30320b59b683b3772d0eb2eff81
Juliette Cordor <juliette@jewelexx.com>
2023-03-15 21:27:02 +1100

    fixes

--- configs/zsh.sh
+++ configs/zsh.sh
@@ -17,7 +17,15 @@ export plugins=(
   sudo
 )
 
-if command -v google-chrome-stable; then
+if command -v sfsu.exe > /dev/null; then
+  source <(sfsu.exe hook --shell zsh)
+fi
+
+if command -v conda > /dev/null; then
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
+fi
+
+if command -v google-chrome-stable > /dev/null; then
   export CHROME_EXECUTABLE="google-chrome-stable"
 else
   export CHROME_EXECUTABLE="chromium"
@@ -211,3 +219,18 @@ end=$(date +%s%N)
 duration="$((end - start))"
 
 echo "Execution time was $((duration / 1000000)) milliseconds"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/juliette/miniconda3/bin/conda' 'shell.zsh' 'hook' 2>/dev/null)"
+if [ $? -eq 0 ]; then
+  eval "$__conda_setup"
+else
+  if [ -f "/home/juliette/miniconda3/etc/profile.d/conda.sh" ]; then
+    . "/home/juliette/miniconda3/etc/profile.d/conda.sh"
+  else
+    export PATH="/home/juliette/miniconda3/bin:$PATH"
+  fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
