3aea7b83a92e678f29d1fcf3df0591a60965c7d0
Juliette Cordor <professional@maybejules.com>
2024-03-17 15:49:11 +1100

    fixes

--- configs/zsh.sh
+++ configs/zsh.sh
@@ -247,3 +247,10 @@ esac
 
 # Fig post block. Keep at the bottom of this file.
 [[ -f "$HOME/.fig/shell/zshrc.post.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.post.zsh"
+
+# bun completions
+[ -s "/home/juliette/.bun/_bun" ] && source "/home/juliette/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
