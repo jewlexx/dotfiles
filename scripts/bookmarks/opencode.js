1f43570e755521191836c84ec8ccd0b335b34cf7
Juliette Cordor <professional@maybejules.com>
2024-09-20 09:16:59 +1000

    added opencode script

---
+++ scripts/bookmarks/opencode.js
@@ -0,0 +1,17 @@
+javascript: {
+  void 0;
+
+  function openCode(gitUrl) {
+    const url = new URL(gitUrl);
+    url.searchParams = new URLSearchParams();
+    url.hash = "";
+
+    window.open(`https://vscode.dev/#${url.toString()}`, "_blank");
+  }
+
+  const url = window.location.href;
+
+  if (url.includes("github.com")) {
+    openCode(url);
+  }
+}
