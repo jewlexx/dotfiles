f3f999ba32a87dd11767e79fdec9e3612d209d7e
Juliette Cordor <professional@maybejules.com>
2024-11-13 10:10:06 +1100

    updates

--- scripts/setup/installers/pacman.ts
+++ scripts/setup/installers/pacman.ts
@@ -3,10 +3,11 @@ import { Installer, type ToolInstaller } from "./index.ts";
 export class PacmanInstaller extends Installer {
   install_tool(tool: ToolInstaller): Promise<Deno.CommandOutput> {
     console.log("Installing tool", tool.package_names.pacman);
-    const installer = new Deno.Command("pacman", {
-      args: ["-S", tool.package_names.pacman!, "--noconfirm"],
+    const installer = new Deno.Command("sudo", {
+      args: ["pacman", "-S", tool.package_names.pacman!, "--noconfirm"],
       stdout: "piped",
       stderr: "piped",
+      stdin: "inherit",
     });
 
     return installer.output();
