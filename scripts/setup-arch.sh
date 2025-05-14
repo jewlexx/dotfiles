5b3c149a5612cf01374a04a159799f8bf52060f9
Juliette <61040004+jewlexx@users.noreply.github.com>
2025-05-15 09:13:21 +1000

    Update setup-arch.sh

--- scripts/setup-arch.sh
+++ scripts/setup-arch.sh
@@ -11,7 +11,7 @@ echo "Installed Rustup"
 
 mkdir /tmp
 cd /tmp || exit
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/paru-bin.git && cd paru-bin && makepkg -si
 cd "$OLD_PWD" || exit
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/loket/oh-my-zsh/feature/batch-mode/tools/install.sh)"
